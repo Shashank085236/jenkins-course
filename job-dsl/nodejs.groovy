de4ab56a9d315d92e4e0617a94144d1c602c858d
Shashank Dwivedi <dwivedi.shashank@outlook.com>
2022-03-19 15:11:06 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,12 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+       // git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+       //     node / gitConfigName('DSL User')
+       //     node / gitConfigEmail('jenkins-dsl@newtech.academy')
+       // }
+        git('https://github.com/Shashank085236/docker-demo.git') { node ->
+              node / gitConfigName('Shashank085236')
+              node / gitConfigEmail('dwivedi.shashank@outlook.com')
         }
     }
     triggers {
